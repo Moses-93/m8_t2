8403c7a354d1183a0786aa1ab364d2461a5ce931
Moses-93 <moieseenko2001@gmail.com>
2024-12-15 18:25:38 +0200

    update config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from os import getenv
 from dotenv import load_dotenv
 import mongoengine as me
+import pika
 
 
 load_dotenv()
@@ -11,3 +12,5 @@ me.connect(
     host=mongo_uri,
     alias="default",
     )
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(rabbitmq_uri))
